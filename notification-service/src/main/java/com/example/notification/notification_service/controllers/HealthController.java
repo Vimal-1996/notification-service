5993e211570da1a271580dd32f2a13034d225144
vimal-1996 <vimal.prof19@gmail.com>
2025-04-02 01:35:17 -0400

    added database design

---
+++ notification-service/src/main/java/com/example/notification/notification_service/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package com.example.notification.notification_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "notification Service is setup and running fine";
+	}
+
+}
